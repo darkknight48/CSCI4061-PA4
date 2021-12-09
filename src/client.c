64f04fc288ad30850f1c8689b4b5d84a5b7d05f7
Andrew Bianchini <bianc064@umn.edu>
2021-12-09 15:00:06 -0600

    time

--- src/client.c
+++ src/client.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]){
     }*/
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("Time: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
